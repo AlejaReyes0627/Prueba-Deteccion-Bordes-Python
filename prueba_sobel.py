42f344d77f851dcb90b3e8c01165b23a05acdd5e
AlejaReyes0627 <aleja.reyes0627@hotmail.com>
2022-10-26 15:52:58 -0500

    no

--- prueba_sobel.py
+++ prueba_sobel.py
@@ -13,7 +13,8 @@ Hy = np.array(([-1, -2, -1], [0, 0, 0], [1, 2, 1])) # Prewitt filter
 #Hx2 = np.array(([0, 1, 0], [1, -4, 1], [0, 1, 0])) LAPLACIANO
 
 #Hx2 = np.array(([2, -1, 2], [-1, -4, -1], [2, -1, 2])) otro laplaciano xD
-Hx2 = np.array(([-1, -1, -1], [-1, 8, -1], [-1, -1, -1]))
+#Hx2 = np.array(([-1, -1, -1], [-1, 8, -1], [-1, -1, -1])) otro laplaciano x3
+Hx2 = np.array(([-1, -1, -1], [0, 0, 0], [1, 1, 1]))
 
 
 SGx = convolve2d(gray, Hx)
