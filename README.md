34289937d62ae3a8eef20f148066ad70c3906166
mariajosevillalba <majosevillalba@live.com>
2022-10-27 11:21:42 -0500

    mejora

---
+++ README.md
@@ -0,0 +1,5 @@
+pip install pillow
+pip install opencv-python
+pip install scipy
+pip install numpy
+pip install matplotlib
--- prueba_canny.py
+++ prueba_canny.py
@@ -6,9 +6,11 @@ original = cv2.imread('lena-std.png')
 # Convertimos a escala de grises
 gris = cv2.cvtColor(original, cv2.COLOR_BGR2GRAY)
 n = input("Put an odd number: ")
+n2 = input("Put the min threeshold: ")
+n1 = input("Put the max threeshold: ")
 # Aplicar suavizado Gaussiano
 gauss = cv2.GaussianBlur(gris, (int(n),int(n)), 0)
 # Detectamos los bordes con Canny
-canny = cv2.Canny(gauss, 50, 150)
+canny = cv2.Canny(gauss, int(n2), int(n1))
 cv2.imshow("canny", canny) 
 cv2.waitKey(0)
