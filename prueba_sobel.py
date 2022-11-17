932e22b3006e8ba96f5cf4a25629c23c38b1d304
AlejaReyes0627 <aleja.reyes0627@hotmail.com>
2022-11-16 22:53:41 -0500

    hola majo x3

--- prueba_sobel.py
+++ prueba_sobel.py
@@ -8,9 +8,10 @@ img = Image.open('herramientas.jpg')
 gray = np.mean(img, axis = 2)
 
 Hx = np.array(([-1, 0, 1], [-2, 0, 2], [-1, 0, 1])) # Sobel Filter
+Hy = np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]]) #transpose even we could reduce it to np.transpose(Hx) but we done the simple way to get it.
 
 SGx = convolve2d(gray, Hx)
-SGy = convolve2d(gray, Hx)
+SGy = convolve2d(gray, Hy)
 
 
 Sobel_out = np.sqrt( np.square(SGx) + np.square(SGy) )
